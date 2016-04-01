cb2956c47c8a7a8b205503733661e8cad5542ad8
Daniel Ortega <danielNakata@users.noreply.github.com>
2016-04-01 15:43:00 -0600

    Primera carga del proyecto web
    primera carga del proyecto web

---
+++ WEBParallax/index.php
@@ -0,0 +1,134 @@
+<!-- Author: Michael Milstead / Mode87.com
+	 for Untame.net
+	 Twitter Bootstrap Tutorial
+	 Parallax Demo, 2013
+-->
+<!doctype html>
+<html class="no-js" lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Daniel Ortega</title>
+	<meta name="description" content="Personal Web Page Java JavaScript HTML CSS Bootstrap JSP">
+	<meta name="author" content="danielnakata">
+
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/estilo.css" rel="stylesheet" type="text/css">
+	<script src="js/jquery-1.6.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/init.js"></script>
+
+</head>
+
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="navbar-inner">
+	  	<div class="container">
+		    <a class="brand" href="#">Daniel Ortega</a>
+		    <ul class="nav">
+		      <li class="active"><a href="#">Home</a></li>
+		      <li><a href="#">Proyectos</a></li>
+		      <li><a href="#">Contacto</a></li>
+		    </ul>
+		</div>
+	  </div>
+	</div>
+
+	<!-- Section #1 -->
+	<section id="intro" data-speed="6" data-type="background">
+		<div class="container">
+			<div class="row-fluid">
+		        <div class="span4 text-center">
+		          <img src="images/desktop.png">
+		        </div>
+		        <div class="span8 text-center">
+		        </div>
+	    	</div>
+	    	<div class="hero-unit">
+	    		<h2>Pagina Personal</h2>
+	    		<p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Nullam pellentesque turpis ex, at posuere justo faucibus non. 
+                            Vestibulum ullamcorper risus non fermentum dictum. 
+                            Sed viverra ac nulla quis pellentesque. Integer sit amet turpis at tellus tempus bibendum. 
+                            Nunc sed tortor nibh. Nunc facilisis urna nec dolor pharetra aliquam. 
+                            Proin euismod arcu vitae neque auctor iaculis. Sed convallis non nunc vel hendrerit. 
+                            Integer pulvinar enim ac fringilla dapibus. Mauris semper, orci ac accumsan laoreet, nibh tortor iaculis sem, 
+                            in dignissim massa dui nec elit. Proin pharetra massa cursus, finibus purus at, volutpat est.
+                        </p>
+	    	</div>
+	    </div>
+	</section>
+
+	<!-- Section #2 -->
+	<section id="home" data-speed="4" data-type="background">
+            <div class="container">
+                <div class="row-fluid">
+		        <div class="span4 well">
+                            <h2>Heading</h2>
+                            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque cursus nisl consectetur et.</p>
+                            <p><a class="btn btn-success" href="#">View details &raquo;</a></p>
+		        </div><!-- /.span4 -->
+		        <div class="span4 well">
+                            <h2>Heading</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                            <p><a class="btn btn-success" href="#">View details &raquo;</a></p>
+		        </div><!-- /.span4 -->
+		        <div class="span4 well">
+                            <h2>Heading</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum odio, dapibus ac in, massa justo sit amet.</p>
+                            <p><a class="btn btn-success" href="#">View details &raquo;</a></p>
+		        </div><!-- /.span4 -->
+	    	</div>
+	    </div>
+	</section>
+
+	<!-- Section #3 -->
+	<section id="about" data-speed="2" data-type="background">
+		<div class="container">
+			<div class="page-header">
+				<h1>Final Section Headline <small>Some subtext goes here...</small></h1>
+			</div>
+			<div class="row-fluid">
+		        <div class="span6">
+		          <h2>More Details</h2>
+		          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque cursus nisl consectetur et.</p>
+		          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque cursus nisl consectetur et.</p>
+		          <p><a class="btn btn-success">View details &raquo;</a></p>
+		        </div><!-- /.span4 -->
+		        <div class="span6">
+		         	<h2>Heading</h2>
+		         	<div class="media">
+						<a class="pull-left" href="#">
+					    	<img class="media-object" src="images/check.png">
+						</a>
+						<div class="media-body">
+						    <h4 class="media-heading">Media heading</h4>
+						    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+						</div>
+					</div>
+		         	<div class="media">
+						<a class="pull-left" href="#">
+					    	<img class="media-object" src="images/check.png">
+						</a>						<div class="media-body">
+						    <h4 class="media-heading">Media heading</h4>
+						    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+						</div>
+					</div>
+		         	<div class="media">
+						<a class="pull-left" href="#">
+					    	<img class="media-object" src="images/check.png">
+						</a>
+						<div class="media-body">
+						    <h4 class="media-heading">Media heading</h4>
+						    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+						</div>
+					</div>
+		        </div><!-- /.span4 -->
+	    	</div>
+	    </div>
+	</section>
+
+</body>
+
+</html>
